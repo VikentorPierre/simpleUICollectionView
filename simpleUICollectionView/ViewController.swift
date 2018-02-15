1128113cd817d6379680885c35953c285ed3d710
Mosdevv <mosDev2017@gmail.com>
2018-02-15 18:55:58 -0500

    Initial Commit

---
+++ simpleUICollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleUICollectionView
+//
+//  Created by Vikentor Pierre on 2/15/18.
+//  Copyright © 2018 Vikentor Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
