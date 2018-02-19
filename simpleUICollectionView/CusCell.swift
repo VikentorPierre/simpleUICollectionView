5b76c9661b4517183dcac42e4bfefcdcae81c3b4
Mosdevv <mosDev2017@gmail.com>
2018-02-19 18:45:38 -0500

    commit

---
+++ simpleUICollectionView/CusCell.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  simpleUICollectionView
+//
+//  Created by Vikentor Pierre on 2/16/18.
+//  Copyright © 2018 Vikentor Pierre. All rights reserved.
+import UIKit
+// this class is our custom uicollectionview cell, it just add an uiimageview into the cell
+class CusCell: UICollectionViewCell{
+    
+    var foodImageView:UIImageView!
+    
+    var food:SeaFood? {
+        didSet{
+            guard let foodimg = food?.image else {return}
+            foodImageView.image = UIImage(named: foodimg)
+        }
+
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCusCellLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCusCellLayout(){
+        foodImageView = UIImageView(frame: contentView.frame)
+        foodImageView.contentMode = .scaleAspectFill
+        foodImageView.clipsToBounds = true
+        //foodImageView.layer.borderWidth = 2
+        //foodImageView.layer.borderColor = UIColor.blue.cgColor
+        
+        contentView.addSubview(foodImageView)
+        
+    }
+    
+}
+
--- simpleUICollectionView/MainViewController.swift
+++ simpleUICollectionView/MainViewController.swift
@@ -1,21 +1,19 @@
-//
 //  ViewController.swift
 //  simpleUICollectionView
 //
 //  Created by Vikentor Pierre on 2/15/18.
 //  Copyright © 2018 Vikentor Pierre. All rights reserved.
 //
-
 import UIKit
 
 class MainViewController: UIViewController {
     
     // when making a collection we first you have to init the uicollectionView and layout
-    var collectionView:UICollectionView! // our uicollectionView var
-    var layout: UICollectionViewFlowLayout! // the layout is gonna be of flowlayout
-    // the SeaFood struct will be the collectionView Datasource
+    var collectionView:UICollectionView!
+    var layout: UICollectionViewFlowLayout!
     
-    let food = [SeaFood(image:"seafood1", type: "seafood"), SeaFood(image:"seafood2", type: "seafood")]
+    // the SeaFood struct will be the collectionView Datasource
+    let food = [SeaFood(image:"seafood1.jpeg", type: "seafood"),SeaFood(image:"seafood2", type: "seafood") ,SeaFood(image:"seafood3", type: "seafood"),SeaFood(image:"seafood4", type: "seafood"),SeaFood(image:"seafood5", type: "seafood"),SeaFood(image:"seafood6", type: "seafood"),SeaFood(image:"fish1", type: "fish"),SeaFood(image:"lobster", type: "lobster"),SeaFood(image:"octopus", type: "octopus")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,24 +26,31 @@ class MainViewController: UIViewController {
         navigationItem.title = "UICollectionView"
         
     }
+    // initializing a collectionview and layout and adding it to the viewController
     func setupCollectionView (){
         layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        
+        //pick the scrollDirection
+        //layout.scrollDirection = .horizontal
+        layout.scrollDirection = .vertical
         
         collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.backgroundColor = .blue
+        collectionView.register(CusCell.self, forCellWithReuseIdentifier: "foodCell")
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         view.addSubview(collectionView)
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
-
 
 struct SeaFood{
     var image:String?
@@ -54,11 +59,25 @@ struct SeaFood{
 }
 
 extension MainViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    
+    //specifying the number of section in the collectionView
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
+    // specifying the number of cell in the given section
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return food.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 250)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! CusCell
+        cell.food = food[indexPath.row]
+        //cell.awakeFromNib()
+        return cell
     }
 }
 
